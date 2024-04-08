38d37b7e504a9f92edcc6fd3b6871a38a542a44c
Nicolas Falla <nicfalla@publicisgroupe.net>
2024-04-08 15:17:46 -0500

    Build v0.1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,7 +5,7 @@ package org.example;
 public class Calculator {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world v0.1");
     }
 
     public double sum(double a, double b){
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -40,7 +40,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(calculator.multiplication(4, 4), 16);
     }
-    
+
     @Test
     public void fiveTimesTwo(){
         Calculator calculator = new Calculator();
