49f802e3390d150511af57a0b6f3753de651323b
Nicolas Falla <nicfalla@publicisgroupe.net>
2024-04-08 15:30:37 -0500

    v0.4

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,7 +5,7 @@ package org.example;
 public class Calculator {
 
     public static void main(String[] args){
-        System.out.println("Hello world v0.3");
+        System.out.println("Hello world v0.4");
     }
 
     public double sum(double a, double b){
